757791960a959d34ed805d04c3999125dfd46b7e
Balázs Gerlei <gbalazs6@gmail.com>
2013-05-19 22:11:19 +0200

    Initial commit
    Created the project and added Caliburn.Micro,
    Caliburn.Micro.BindableAppBar, and Windows Phone Toolkit references.
    
    - The MainPage contains a tile view, similar to the HERE City Lens app.

---
+++ HappanedHere/HappanedHere/AppBootstrapper.cs
@@ -0,0 +1,48 @@
+﻿using HappanedHere.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+
+namespace HappanedHere
+{
+    public class AppBootstrapper : PhoneBootstrapper
+    {
+        PhoneContainer container;
+
+        protected override void Configure()
+        {
+            container = new PhoneContainer(RootFrame);
+
+            if (!Execute.InDesignMode)
+                container.RegisterPhoneServices();
+
+            container.PerRequest<MainPageViewModel>();
+            //container.PerRequest<PivotPageViewModel>();
+            //container.PerRequest<TabViewModel>();
+
+            AddCustomConventions();
+        }
+
+        static void AddCustomConventions()
+        {
+            //ellided  
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            container.BuildUp(instance);
+        }
+    }  
+}
---
+++ HappanedHere/HappanedHere/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace HappanedHere.ViewModels
+{
+
+    public class MainPageViewModel
+    {
+        readonly INavigationService navigationService;
+
+        public MainPageViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        /*public void GotoPageTwo()
+        {
+            navigationService.UriFor<MainPageViewModel>()
+                .WithParam(x => x.NumberOfTabs, 5)
+                .Navigate();
+        }*/
+    }
+}
